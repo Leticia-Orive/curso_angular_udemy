b462d09743edb8b1d4baf46b7917502479faa8bb
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-12-02 16:19:40 +0100

    validar un task

--- 02-task-list-app/src/app/components/task-list/task-list.component.ts
+++ 02-task-list-app/src/app/components/task-list/task-list.component.ts
@@ -12,20 +12,27 @@ export class TaskListComponent {
   public taskListPending: ITask[] = [];
   public taskListCompleted: ITask[] = [];
   public showInputTask: boolean = false;
+  public errorInputTask: boolean = false;
 
   showInputTextTask(){
     this.showInputTask = true;
   }
   createTask(description: string){
-    const task: ITask ={
-      date: new Date(),//La fecha de ahora mismo
-      description, //description: description es lo mismo
-      completed: false
+    if(description.trim()){
+      const task: ITask ={
+        date: new Date(),//La fecha de ahora mismo
+        description: description.trim(), //description: description es lo mismo
+        completed: false
+      }
+      this.taskListPending.push(task);
+      this.showInputTask = false;
+      //Mostramos las tareas pendientes
+      console.log(this.taskListPending);
+      this.errorInputTask = false;//cuando la queremos elimiar sin que halla ningun problema
+    }else{
+      this.errorInputTask = true;
     }
-    this.taskListPending.push(task);
-    this.showInputTask = false;
-    //Mostramos las tareas pendientes
-    console.log(this.taskListPending);
-  }
+    }
+    
 
 }
