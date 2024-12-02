a77b8f267e07b0eba1e87a6ab303fa0979973c81
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-12-02 15:29:37 +0100

    agregar un task

--- 02-task-list-app/src/app/components/task-list/task-list.component.ts
+++ 02-task-list-app/src/app/components/task-list/task-list.component.ts
@@ -11,5 +11,21 @@ export class TaskListComponent {
 
   public taskListPending: ITask[] = [];
   public taskListCompleted: ITask[] = [];
+  public showInputTask: boolean = false;
+
+  showInputTextTask(){
+    this.showInputTask = true;
+  }
+  createTask(description: string){
+    const task: ITask ={
+      date: new Date(),//La fecha de ahora mismo
+      description, //description: description es lo mismo
+      completed: false
+    }
+    this.taskListPending.push(task);
+    this.showInputTask = false;
+    //Mostramos las tareas pendientes
+    console.log(this.taskListPending);
+  }
 
 }
