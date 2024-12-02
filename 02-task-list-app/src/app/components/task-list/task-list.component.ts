edd12b1bf21b35ee2fab6f7bb641d18495e781a4
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-12-02 13:57:54 +0100

    componente taskList

--- 02-task-list-app/src/app/components/task-list/task-list.component.ts
+++ 02-task-list-app/src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITask } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-list',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
+
+  public taskListPending: ITask[] = [];
+  public taskListCompleted: ITask[] = [];
 
 }
