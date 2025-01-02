e2dbc86e88caf14addc94921f2b273903eb68d1a
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-02 13:47:45 +0100

    creando nuestras rutas

--- 03-list-contacts-app/src/app/app.routes.ts
+++ 03-list-contacts-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { ListContactsComponent } from './components/list-contacts/list-contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Vamos a añadir las rutas de los componentes
+    { path: 'add-contact', component: AddContactComponent },
+    {path: 'list-contacts', component: ListContactsComponent},
+    //ruta no valida
+    {path: '**', redirectTo: 'add-contact'}
+];
