953ff2be3a728f9764b33cf2fd13972a5df1fcd4
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-02 17:11:34 +0100

    redirigir a otra ruta usando router

--- 03-list-contacts-app/src/app/components/add-contact/add-contact.component.ts
+++ 03-list-contacts-app/src/app/components/add-contact/add-contact.component.ts
@@ -3,6 +3,7 @@ import { IContact } from '../../models/contact.model';
 import { FormsModule } from '@angular/forms';
 import {  NgClass } from '@angular/common';
 import { ContactService } from '../../services/contact.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
@@ -19,6 +20,7 @@ export class AddContactComponent {
   };
   //pero nosotros lo vamos a hacer de otra forma usando Injectable 
   private contactService: ContactService = inject(ContactService);
+  private router = inject(Router);
   //esta es la forma clasica de hacerlo
   //constructor(private contactService: ContactService){}
 // para importa que se recarge la pagina se debe importar el formsModule
@@ -29,6 +31,10 @@ addContact(){
   //Sirve para hacer una copia de un objeto vacio {} y le añade los valores de this.contact
   const contact = Object.assign({}, this.contact);
   this.contactService.addContact(contact);
-  console.log(this.contactService.listContacts);
+  //navigate es una funcion que nos permite navegar a una ruta especificas cuando tiene propiedades por ejempo 
+  //navigate(['/ruta', {parametro: valor}])
+  //navigateByUrl es una funcion que nos permite navegar a una ruta especifica cuando no tiene propiedades
+  this.router.navigateByUrl('list-contacts');
+  
 }
 }
