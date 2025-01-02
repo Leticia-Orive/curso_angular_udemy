63465f5b79999de11c8faf9ffb5c3d7630ab613b
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-02 13:36:10 +0100

    creando nuestro componente

---
+++ 03-list-contacts-app/src/app/components/list-contacts/list-contacts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-contacts',
+  imports: [],
+  templateUrl: './list-contacts.component.html',
+  styleUrl: './list-contacts.component.scss'
+})
+export class ListContactsComponent {
+
+}
