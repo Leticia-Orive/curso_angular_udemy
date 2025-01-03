8e75e14cea4435156d0f24924d87824b9350d08b
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-03 09:57:49 +0100

    interface cocktails

---
+++ 04-cocktail-app/src/app/models/cocktail.model.ts
@@ -0,0 +1,9 @@
+export interface ICocktail {
+    id: string,
+    name: string,
+    img: string,
+    glass: string,
+    ingredients: string[],
+    intructions: string,
+    measures: string[],
+}
