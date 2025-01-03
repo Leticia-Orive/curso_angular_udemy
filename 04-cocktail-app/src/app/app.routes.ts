20bf6ef29dcebb53e224255ff28a455099a3d0a3
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-03 08:40:43 +0100

    creando nuestras rutas

--- 04-cocktail-app/src/app/app.routes.ts
+++ 04-cocktail-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListCocktailsComponent } from './components/list-cocktails/list-cocktails.component';
+import { DetailCocktailsComponent } from './components/detail-cocktails/detail-cocktails.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'list-cocktails', component: ListCocktailsComponent},
+    {path: 'detail-cocktail/:id', component: DetailCocktailsComponent}, //los :id son parametros dinamicos
+    {path: '**',  redirectTo: 'list-cocktails'}
+];
