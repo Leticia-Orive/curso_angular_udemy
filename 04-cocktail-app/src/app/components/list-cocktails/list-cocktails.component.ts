81347bef1dd7ab9b2d400abd5020d921fa1b55f5
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-03 09:53:29 +0100

    crear formulario filtro

--- 04-cocktail-app/src/app/components/list-cocktails/list-cocktails.component.ts
+++ 04-cocktail-app/src/app/components/list-cocktails/list-cocktails.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IFilter } from '../../models/filter.model';
 
 @Component({
   selector: 'app-list-cocktails',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './list-cocktails.component.html',
   styleUrl: './list-cocktails.component.scss'
 })
 export class ListCocktailsComponent {
+  public filter: IFilter = {
+    searchBy: 'name', 
+    value: ''
+  };
+
+  filterData(){
+    console.log(this.filter);
+  }
 
 }
