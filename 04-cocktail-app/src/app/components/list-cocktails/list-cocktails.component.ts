528a14f5b6aa0593d1b1202f48d4045ba99ecdb2
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-03 13:46:48 +0100

    mostrar spinner mientras cargamos cocktail

--- 04-cocktail-app/src/app/components/list-cocktails/list-cocktails.component.ts
+++ 04-cocktail-app/src/app/components/list-cocktails/list-cocktails.component.ts
@@ -4,6 +4,7 @@ import { IFilter } from '../../models/filter.model';
 import { CocktailService } from '../../services/cocktail.service';
 import { ICocktail } from '../../models/cocktail.model';
 import { RouterLink } from '@angular/router';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-list-cocktails',
@@ -17,6 +18,7 @@ export class ListCocktailsComponent {
 
   public listCocktails: ICocktail[] = [];
   public searched: boolean = false;
+  public loadCocktails: boolean = true;
 
   public filter: IFilter = {
     searchBy: 'name', 
@@ -26,19 +28,24 @@ export class ListCocktailsComponent {
   filterData(){
     console.log(this.filter);
     this.searched = true;
-    this.cocktailService.getCocktails(this.filter).subscribe({
-      next: (ListCocktails: ICocktail[]) => {
-        this.listCocktails = ListCocktails;
-      },
-      error: (error) =>{
-        console.error(error);
+    this.loadCocktails = false;
 
-      },
-      complete: () => {
-        
-      }
-
-    });
+      this.cocktailService.getCocktails(this.filter).subscribe({
+        next: (ListCocktails: ICocktail[]) => {
+          this.listCocktails = ListCocktails;
+        },
+        error: (error) =>{
+          console.error(error);
+  
+        },
+        complete: () => {
+          this.loadCocktails = true;
+          
+        }
+  
+      });
+    
+    
   }
 
 }
