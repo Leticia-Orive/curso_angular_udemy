33e1aabfbc7964b8a22c08dd6f1c24b003920dc3
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-07 11:45:13 +0100

    contenedor base

--- 05-dreams-trips-app/src/app/app.component.ts
+++ 05-dreams-trips-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { ListCountriesComponent } from './components/list-countries/list-countries.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListCountriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
