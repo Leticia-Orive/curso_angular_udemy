a91fbd499c0dcee7e9e7b69f34a656f018b69c28
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-07 12:42:27 +0100

    OBTENER SUBREGIONES DESDE UN JSON

--- 05-dreams-trips-app/src/app/components/list-countries/list-countries.component.ts
+++ 05-dreams-trips-app/src/app/components/list-countries/list-countries.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-countries',
@@ -7,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './list-countries.component.scss'
 })
 export class ListCountriesComponent {
+  private countryService = inject(CountryService);
+
+  public subregions$: Observable<string[]> = this.countryService.getAllSubregions();
 
 }
--- 05-dreams-trips-app/src/app/services/country.service.ts
+++ 05-dreams-trips-app/src/app/services/country.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,8 @@ export class CountryService {
 
   private URL_BASE = 'https://restcountries.com/v3.1';
   private http = inject(HttpClient);
+
+  getAllSubregions(){
+    return this.http.get<string[]>(`/data/subregion.json`).pipe(first());
+  };
 }
