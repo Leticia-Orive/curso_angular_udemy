11a79228551f469ab376c7c76639a6f7ad5012cb
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-08 16:01:24 +0100

    rutas

--- 06-reservations-app/src/app/app.routes.ts
+++ 06-reservations-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AddBookingComponent } from './components/add-booking/add-booking.component';
+import { EventsCalendarComponent } from './components/events-calendar/events-calendar.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'add-booking', component: AddBookingComponent},
+    {path:'login', component: LoginComponent},
+    {path:'logout', component: LogoutComponent},
+    {path:'events-calendar', component: EventsCalendarComponent},
+    {path: '**', redirectTo: 'add-booking'}
+];
