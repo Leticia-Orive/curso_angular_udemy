6bb5983f614be59a9cd206af76eeb46b01027267
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-08 17:09:10 +0100

    preparamos el header

--- 06-reservations-app/src/app/components/header/header.component.ts
+++ 06-reservations-app/src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, TranslateModule, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
