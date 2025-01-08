50f402ba04421052628e18f7b0d46ec8cdebf4a9
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-08 12:29:27 +0100

    mostrar markers en el mapa

--- 05-dreams-trips-app/src/app/components/list-countries/components/map-countries/map-countries.component.ts
+++ 05-dreams-trips-app/src/app/components/list-countries/components/map-countries/map-countries.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import { Component, Input, } from '@angular/core';
+import { GoogleMap, MapAdvancedMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map-countries',
-  imports: [GoogleMap],
+  imports: [GoogleMap, MapAdvancedMarker],
   templateUrl: './map-countries.component.html',
   styleUrl: './map-countries.component.scss'
 })
 export class MapCountriesComponent {
+
+  @Input() markers: google.maps.LatLngLiteral[] = [];
   public center: google.maps.LatLngLiteral = {lat: 0, lng: 0};
 
 
