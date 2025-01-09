c2778996c6a2878e193e814c45c64f4319d7f465
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-09 17:06:19 +0100

    login

--- 06-reservations-app/src/app/components/login/login.component.ts
+++ 06-reservations-app/src/app/components/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { IAuth } from '../../models/auth.model';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +15,10 @@ import { IAuth } from '../../models/auth.model';
 export class LoginComponent {
 
   private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private toastrService = inject(ToastrService);
+  private translateService = inject(TranslateService);
+  private router = inject(Router);
 
   public formGroup: FormGroup = new FormGroup({});
 
@@ -27,6 +34,23 @@ export class LoginComponent {
   login(){
     const auth: IAuth = this.formGroup.value;
     console.log(auth);
+
+    this.authService.login(auth).subscribe( {
+      next: (success) => {
+        if(success){
+          this.toastrService.success(
+            this.translateService.instant('login.success'),
+            this.translateService.instant('success')
+          );
+          this.router.navigateByUrl('events-calendar');
+        } else {
+          this.toastrService.error(
+            this.translateService.instant('login.error'),
+            this.translateService.instant('error')
+          );
+        }
+      }
+    });
   }
 
 }
--- 06-reservations-app/src/app/services/auth.service.ts
+++ 06-reservations-app/src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { IAuth } from '../models/auth.model';
+import { first, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  login(authCredentials: IAuth){
+    let success = false;
+    if(authCredentials.email === 'drr@gmail.com' && authCredentials.password === '123456'){
+      success = true;
+      localStorage.setItem('isLogged', 'true');
+    }
+    return of(success).pipe(first());
+  }
 }
