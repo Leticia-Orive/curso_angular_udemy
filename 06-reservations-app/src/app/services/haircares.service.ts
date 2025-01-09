3cbaed09f82b828c35f1f55c937e91d96a260d99
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-09 09:07:56 +0100

    enviroments

--- 06-reservations-app/src/app/services/haircares.service.ts
+++ 06-reservations-app/src/app/services/haircares.service.ts
@@ -4,6 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HaircaresService {
-
-  constructor() { }
+  private URL_BASE = '${environment.urlServer}/haircares';
 }
