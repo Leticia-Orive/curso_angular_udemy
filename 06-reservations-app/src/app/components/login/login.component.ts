e26c56b0ba058d5c17888e81a927dda5a4863a78
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-09 16:53:14 +0100

    formulario login

--- 06-reservations-app/src/app/components/login/login.component.ts
+++ 06-reservations-app/src/app/components/login/login.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { IAuth } from '../../models/auth.model';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, TranslateModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private formBuilder = inject(FormBuilder);
+
+  public formGroup: FormGroup = new FormGroup({});
+
+  ngOnInit(){
+
+    this.formGroup = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+
+  }
+
+  login(){
+    const auth: IAuth = this.formGroup.value;
+    console.log(auth);
+  }
 
 }
