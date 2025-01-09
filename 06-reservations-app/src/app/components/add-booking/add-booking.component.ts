f25b057971344ac8b8fbc4c33ab465bf84c16654
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-09 08:59:42 +0100

    formulario reactive

--- 06-reservations-app/src/app/components/add-booking/add-booking.component.ts
+++ 06-reservations-app/src/app/components/add-booking/add-booking.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-add-booking',
-  imports: [TranslateModule],
+  standalone: true,
+  imports: [TranslateModule, ReactiveFormsModule],
   templateUrl: './add-booking.component.html',
-  styleUrl: './add-booking.component.scss'
+  styleUrl: './add-booking.component.scss',
+  providers: [
+    DatePipe
+  ]
 })
 export class AddBookingComponent {
 
-}
+  public formBuilder: FormBuilder = inject(FormBuilder);
+  
+  public datePipe: DatePipe = inject(DatePipe);
+
+  public formGroup: FormGroup = new FormGroup({});
+
+  ngOnInit() {
+
+    this.formGroup = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      date: new FormControl(this.datePipe.transform(new Date(), 'yyyy-MM-dd'), Validators.required),
+      time: new FormControl('', Validators.required),
+      haircare: new FormControl('', Validators.required)
+    })
+  }
+  addBooking(){
+    console.log(this.formGroup.value);
+  }
+
+  }
