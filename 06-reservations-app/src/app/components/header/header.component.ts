8bf2733c19751690b6e34fdc7f1d2784854fbf47
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-09 17:36:03 +0100

    comprobar si estamos logeados usando subject

--- 06-reservations-app/src/app/components/header/header.component.ts
+++ 06-reservations-app/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AuthService } from '../../services/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, TranslateModule, RouterLinkActive],
+  imports: [RouterLink, TranslateModule, RouterLinkActive, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -12,8 +14,10 @@ export class HeaderComponent {
 
   //cambiar idioma
   private translateService = inject(TranslateService);
+  private authService = inject(AuthService);
 
   public languages: string[] = ['en', 'es'];
+  public isAutheticated$ = this.authService.isAutheticated$;
 //cambiar idioma
   changeLanguage(language: string){
     this.translateService.use(language);
--- 06-reservations-app/src/app/services/auth.service.ts
+++ 06-reservations-app/src/app/services/auth.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import { IAuth } from '../models/auth.model';
-import { first, of } from 'rxjs';
+import { BehaviorSubject, first, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  /*Esto es para que el servicio sea observable
+  private subjectAuth = new Subject<boolean>().pipe(
+    startWith( ()=> localStorage.getItem('isLogged') == 'true' )
+  );*/
+
+  private subjectAuth = new BehaviorSubject<boolean>(localStorage.getItem('isLogged') == 'true')
+  /*Es con $ al final, enseguida me doy cuenta de ello*/ 
+  public isAutheticated$ = this.subjectAuth.asObservable();
+
   login(authCredentials: IAuth){
     let success = false;
     if(authCredentials.email === 'drr@gmail.com' && authCredentials.password === '123456'){
       success = true;
       localStorage.setItem('isLogged', 'true');
+      /** Estamos logueados o no */
+      this.subjectAuth.next(true);
     }
     return of(success).pipe(first());
   }
