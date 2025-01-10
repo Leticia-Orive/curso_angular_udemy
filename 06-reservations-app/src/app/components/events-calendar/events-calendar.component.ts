44766febbfd758c923e358a34c6a68d217a26845
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-10 11:41:17 +0100

    cambio de idioma de el calendario

--- 06-reservations-app/src/app/components/events-calendar/events-calendar.component.ts
+++ 06-reservations-app/src/app/components/events-calendar/events-calendar.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core/index.js';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-events-calendar',
+  standalone: true,
   imports: [FullCalendarModule],
   templateUrl: './events-calendar.component.html',
   styleUrl: './events-calendar.component.scss'
 })
 export class EventsCalendarComponent {
 
+  private translateService = inject(TranslateService)
+
   public calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-   plugins: [dayGridPlugin],
-   firstDay: 1,
-   locale: 'es',
-   headerToolbar: {
-     left: 'title',
-     right: 'prev,next'
-   },
+    plugins: [dayGridPlugin],
+    firstDay: 1,
+    locale: this.translateService.currentLang,
+    headerToolbar: {
+      left: 'title',
+      right: 'prev,next'
+    }
+  };
 
-};
+  ngOnInit() {
+
+    this.translateService.onLangChange.asObservable().subscribe({
+      next: (event: LangChangeEvent) => {
+        this.calendarOptions.locale = event.lang;
+        console.log(event.lang);
+      }
+    })
+
+
+  }
+
 }
