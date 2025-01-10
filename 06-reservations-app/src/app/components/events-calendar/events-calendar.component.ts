81947134ad407e1e5bbb8361f09e6ba1b7f5907a
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-10 10:37:51 +0100

    instalar fullcalendar de fullcalendar.io

--- 06-reservations-app/src/app/components/events-calendar/events-calendar.component.ts
+++ 06-reservations-app/src/app/components/events-calendar/events-calendar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core/index.js';
+import dayGridPlugin from '@fullcalendar/daygrid';
 @Component({
   selector: 'app-events-calendar',
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './events-calendar.component.html',
   styleUrl: './events-calendar.component.scss'
 })
 export class EventsCalendarComponent {
 
+  public calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+   plugins: [dayGridPlugin]
+
+};
 }
