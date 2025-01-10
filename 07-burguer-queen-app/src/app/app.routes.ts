834b634f4823dd532154f496c12c99c71afde203
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2025-01-10 14:24:59 +0100

    rutas

--- 07-burguer-queen-app/src/app/app.routes.ts
+++ 07-burguer-queen-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { PayOrderComponent } from './components/pay-order/pay-order.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'categories', component: CategoriesComponent },
+    { path: 'products/:categoryId', component: ProductsComponent },
+    { path: 'product/:id', component: ProductComponent },
+    { path: 'pay-order', component: PayOrderComponent },
+    { path: '**', redirectTo: 'categories' }
+];
