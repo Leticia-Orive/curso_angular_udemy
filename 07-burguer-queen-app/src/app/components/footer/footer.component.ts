f333adcf6e5afc6518f5b18c79d45cc7cec23314
Leticia Orive <leticia.orive@accenture.com>
2025-01-13 11:37:03 +0100

    footer

--- 07-burguer-queen-app/src/app/components/footer/footer.component.ts
+++ 07-burguer-queen-app/src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [TranslateModule, DatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  //para que se cambie automaticamente el año
+  public today: Date = new Date();
 
 }
