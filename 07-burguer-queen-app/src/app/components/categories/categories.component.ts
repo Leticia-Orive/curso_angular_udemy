63ff3f8868ab6045483cfe92a181361ff30e34b8
Leticia Orive <leticia.orive@accenture.com>
2025-01-13 13:42:32 +0100

    mostrar nuestras categorias usando MatCard

--- 07-burguer-queen-app/src/app/components/categories/categories.component.ts
+++ 07-burguer-queen-app/src/app/components/categories/categories.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
+import { Observable } from 'rxjs';
+import { ICategory } from '../../models/category.model';
+import { AsyncPipe } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [AsyncPipe, MatCardModule, TranslateModule, RouterLink],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+
+  private categoriesService = inject(CategoriesService);
+
+  public categories$: Observable<ICategory[]> = this.categoriesService.getCategories();
 
 }
