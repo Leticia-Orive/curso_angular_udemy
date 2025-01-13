3e6ffc707cfeffb795eea4fd0b531a98511d9a32
Leticia Orive <leticia.orive@accenture.com>
2025-01-13 11:10:01 +0100

    header de nuestra app

--- 07-burguer-queen-app/src/app/components/header/header.component.ts
+++ 07-burguer-queen-app/src/app/components/header/header.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  private translateService= inject(TranslateService);
+
+  public languages: string[] = ['es', 'en'];
+
+  changeLang(language:string){
+    this.translateService.use(language);
+  }
 
 }
