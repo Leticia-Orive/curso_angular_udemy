7a17643d5d11abee22eeaec3a8cf75c331d9b34b
Leticia Orive <leticia.orive@accenture.com>
2025-01-13 11:19:18 +0100

    MatIcon t MatBadge

--- 07-burguer-queen-app/src/app/components/header/header.component.ts
+++ 07-burguer-queen-app/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule,MatBadgeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
