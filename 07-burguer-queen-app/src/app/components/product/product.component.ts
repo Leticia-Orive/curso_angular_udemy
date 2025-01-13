6db692541880a0c6cc60523f4d4be94a3d9328c8
Leticia Orive <leticia.orive@accenture.com>
2025-01-13 16:13:24 +0100

    obtener un producto dado un id

--- 07-burguer-queen-app/src/app/components/product/product.component.ts
+++ 07-burguer-queen-app/src/app/components/product/product.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, first } from 'rxjs';
+import { IProduct } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+
+  private activatedRoute = inject(ActivatedRoute);
+  private productsService = inject(ProductsService);
+
+  public product$: Observable<IProduct> = new Observable<IProduct>();
+
+  ngOnInit(){
+    this.activatedRoute.params.pipe(first()).subscribe({
+      next: (params: Params) => {
+        const productId = params['id'];
+        this.product$ = this.productsService.getProduct(productId);
+      }
+    })
+  }
 
 }
