d9a6fc75bb1656150a7cf608946d57660a8844d3
Leticia Orive <leticia.orive@accenture.com>
2025-01-13 13:20:06 +0100

    obtener nuestras categorias

--- 07-burguer-queen-app/src/app/services/categories.service.ts
+++ 07-burguer-queen-app/src/app/services/categories.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { ICategory } from '../models/category.model';
+import { first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  private URL_BASE = `${environment.urlServer}/v1/categories`;
+  private http = inject(HttpClient)
+
+  getCategories() {
+    return this.http.get<ICategory[]>(this.URL_BASE).pipe(first());
+  }
 }
