7d3f4aec4cbe351789f476ecd6d7b663fe709e5f
Leticia Orive <leticia.orive@accenture.com>
2025-01-14 17:51:56 +0100

    matSnackBar

--- 07-burguer-queen-app/src/app/components/product/product.component.ts
+++ 07-burguer-queen-app/src/app/components/product/product.component.ts
@@ -4,7 +4,7 @@ import { Observable, first } from 'rxjs';
 import { IProduct, IProductExtraOption } from '../../models/product.model';
 import { ProductsService } from '../../services/products.service';
 import { AsyncPipe, JsonPipe, Location, NgClass } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MatDivider } from '@angular/material/divider';
 import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
@@ -15,6 +15,7 @@ import {MatRadioChange, MatRadioModule} from '@angular/material/radio';
 import { ExtraSelectedPipe } from '../../pipes/extra-selected.pipe';
 import { CalculateTotalPricePipe } from '../../pipes/calculate-total-price.pipe';
 import { UserOrderService } from '../../services/user-order.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-product',
   standalone: true,
@@ -30,6 +31,8 @@ export class ProductComponent {
   private location: Location = inject(Location);
   private UserOrderService = inject(UserOrderService);
   private router = inject(Router);
+  private SnackBar = inject(MatSnackBar);
+  private translateService = inject(TranslateService);
 
   public product$: Observable<IProduct> = new Observable<IProduct>();
   public quantitySignal: WritableSignal<number> = signal(1);
@@ -47,6 +50,15 @@ export class ProductComponent {
     console.log(product)
     this.UserOrderService.addProduct(product, this.quantitySignal());
     console.log(this.UserOrderService.productsSignals());
+    this.SnackBar.open(
+      this.translateService.instant('label.add.product.success'),
+      this.translateService.instant('label.ok'),
+      {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 5000
+      }
+    );
     this.router.navigateByUrl('/categories');
   }
   
