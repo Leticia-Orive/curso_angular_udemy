e6cea7d733313d5596f7d5df58baeb6eace00299
Leticia Orive <leticia.orive@accenture.com>
2025-01-14 08:55:38 +0100

    mostrar un producto con extras

--- 07-burguer-queen-app/src/app/components/product/product.component.ts
+++ 07-burguer-queen-app/src/app/components/product/product.component.ts
@@ -3,16 +3,17 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Observable, first } from 'rxjs';
 import { IProduct } from '../../models/product.model';
 import { ProductsService } from '../../services/products.service';
-import { AsyncPipe, Location } from '@angular/common';
+import { AsyncPipe, Location, NgClass } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatDivider } from '@angular/material/divider';
 import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [AsyncPipe, TranslateModule, MatDivider, MatMiniFabButton, MatIcon, MatButton],
+  imports: [AsyncPipe, TranslateModule, MatDivider, MatMiniFabButton, MatIcon, MatButton, NgClass,MatCard, MatCardContent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
