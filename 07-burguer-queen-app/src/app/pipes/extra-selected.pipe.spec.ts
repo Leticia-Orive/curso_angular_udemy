d12a5bf1e49a70980d3eecd00aff250d89b13804
Leticia Orive <leticia.orive@accenture.com>
2025-01-14 10:35:32 +0100

    pipe para seleccionar la opcion del producto

---
+++ 07-burguer-queen-app/src/app/pipes/extra-selected.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ExtraSelectedPipe } from './extra-selected.pipe';
+
+describe('ExtraSelectedPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ExtraSelectedPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
