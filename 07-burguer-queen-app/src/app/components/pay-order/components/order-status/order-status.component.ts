4d6161abeb45866d6c2ba8fb0b54c2accdfe585e
Leticia Orive <leticia.orive@accenture.com>
2025-01-15 13:41:48 +0100

    actualidad cantidades de nuestro order

--- 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
@@ -3,7 +3,10 @@ import { UserOrderService } from '../../../../services/user-order.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatTable, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRowDef, MatHeaderRow, MatRow, MatRowDef, MatCellDef } from '@angular/material/table';
 import { CalculateTotalPricePipe } from '../../../../pipes/calculate-total-price.pipe';
-
+import { MatMiniFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { IProduct } from '../../../../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-status',
@@ -18,10 +21,9 @@ import { CalculateTotalPricePipe } from '../../../../pipes/calculate-total-price
     MatRow,
     MatRowDef,
     MatCellDef,
-    CalculateTotalPricePipe
-
-
-
+    CalculateTotalPricePipe,
+    MatMiniFabButton,
+    MatIcon
   ],
   templateUrl: './order-status.component.html',
   styleUrl: './order-status.component.scss'
@@ -30,9 +32,23 @@ import { CalculateTotalPricePipe } from '../../../../pipes/calculate-total-price
 export class OrderStatusComponent {
 
   private UserOrderService= inject(UserOrderService) ;
+  //injecto el router para poder navegar a la pagina de categorias
+  private router = inject(Router);
 
   public productsSignal = this.UserOrderService.productsSignals;
   public totalOrderSignal = this.UserOrderService.totalProductsSignals;
   public displayedColumns: string[] = ['name', 'price', 'quantity', 'total'];
+
+  oneLessProduct(product: IProduct) {
+    this.UserOrderService.oneLessProduct(product);
+
+    //por si el producto se queda a cero y desaparece que nos derive a la pagina de categorias
+    if(this.productsSignal().length == 0){
+      this.router.navigateByUrl('/categories')
+    }
+  }
+  oneMoreProduct(product: IProduct) {
+    this.UserOrderService.oneMoreProduct(product);
+  }
 
 }
