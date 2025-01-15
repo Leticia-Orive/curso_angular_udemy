d451f8caadf5880139561cb6c0671ec5cbd3bf7a
Leticia Orive <leticia.orive@accenture.com>
2025-01-15 10:20:06 +0100

    Actualizando el numero de productos de nuestro orden

--- 07-burguer-queen-app/src/app/components/header/header.component.ts
+++ 07-burguer-queen-app/src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
+import { UserOrderService } from '../../services/user-order.service';
 
 @Component({
   selector: 'app-header',
@@ -13,8 +14,10 @@ import {MatBadgeModule} from '@angular/material/badge';
 export class HeaderComponent {
 
   private translateService= inject(TranslateService);
+  private userOrderService= inject(UserOrderService);
 
   public languages: string[] = ['es', 'en'];
+  public numProductsSignal: Signal<number> = this.userOrderService.numProductsSignals;
 
   changeLang(language:string){
     this.translateService.use(language);
