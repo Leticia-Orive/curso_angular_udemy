120d4273ff788898bbb77960e7d926a70d260142
Leticia Orive <leticia.orive@accenture.com>
2025-01-15 13:11:34 +0100

    mostrar producto con matTable

--- 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
@@ -1,19 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { UserOrderService } from '../../../../services/user-order.service';
 import { TranslateModule } from '@ngx-translate/core';
-import { JsonPipe } from '@angular/common';
+import { MatTable, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRowDef, MatHeaderRow, MatRow, MatRowDef, MatCellDef } from '@angular/material/table';
+import { CalculateTotalPricePipe } from '../../../../pipes/calculate-total-price.pipe';
+
 
 @Component({
   selector: 'app-order-status',
-  imports: [TranslateModule, JsonPipe],
+  imports: [
+    TranslateModule,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatCellDef,
+    CalculateTotalPricePipe
+
+
+
+  ],
   templateUrl: './order-status.component.html',
   styleUrl: './order-status.component.scss'
 })
+//este se pone en import pero como lo he comentado en html lo quito JsonPipe import { JsonPipe } from '@angular/common';
 export class OrderStatusComponent {
 
   private UserOrderService= inject(UserOrderService) ;
 
   public productsSignal = this.UserOrderService.productsSignals;
   public totalOrderSignal = this.UserOrderService.totalProductsSignals;
+  public displayedColumns: string[] = ['name', 'price', 'quantity', 'total'];
 
 }
