14046616eae19c4191c56d3a426484a20b1a674c
Leticia Orive <leticia.orive@accenture.com>
2025-01-15 10:55:39 +0100

    guardar para pay

--- 07-burguer-queen-app/src/app/app.routes.ts
+++ 07-burguer-queen-app/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { CategoriesComponent } from './components/categories/categories.componen
 import { PayOrderComponent } from './components/pay-order/pay-order.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
+import { payOrderGuard } from './guards/pay-order.guard';
 
 export const routes: Routes = [
     { path: 'categories', component: CategoriesComponent },
     { path: 'products/:categoryId', component: ProductsComponent },
     { path: 'product/:id', component: ProductComponent },
-    { path: 'pay-order', component: PayOrderComponent },
+    { path: 'pay-order', component: PayOrderComponent, canActivate: [payOrderGuard] },
     { path: '**', redirectTo: 'categories' }
 ];
