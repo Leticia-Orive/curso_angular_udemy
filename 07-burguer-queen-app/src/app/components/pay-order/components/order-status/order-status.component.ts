a14be86583a9115405fa86d653814077512bf768
Leticia Orive <leticia.orive@accenture.com>
2025-01-15 12:26:57 +0100

    estado de la orden

--- 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserOrderService } from '../../../../services/user-order.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-status',
-  imports: [],
+  imports: [TranslateModule, JsonPipe],
   templateUrl: './order-status.component.html',
   styleUrl: './order-status.component.scss'
 })
 export class OrderStatusComponent {
+
+  private UserOrderService= inject(UserOrderService) ;
+
+  public productsSignal = this.UserOrderService.productsSignals;
+  public totalOrderSignal = this.UserOrderService.totalProductsSignals;
 
 }
