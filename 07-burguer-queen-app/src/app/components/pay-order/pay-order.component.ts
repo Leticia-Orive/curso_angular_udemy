8dc77c317103ccb443d66115dc49ce9e4931e044
Leticia Orive <leticia.orive@accenture.com>
2025-01-15 12:02:40 +0100

    pasos para pagar con MatStepper

--- 07-burguer-queen-app/src/app/components/pay-order/pay-order.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/pay-order.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
+import {MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious} from '@angular/material/stepper';
+
+import { TranslateModule } from '@ngx-translate/core';
+import { OrderStatusComponent } from "./components/order-status/order-status.component";
 @Component({
   selector: 'app-pay-order',
-  imports: [],
+  imports: [
+    TranslateModule,
+    MatStepper,
+    MatStep,
+    MatStepLabel,
+    MatStepperNext,
+    MatStepperPrevious,
+    MatButton,
+    OrderStatusComponent
+],
   templateUrl: './pay-order.component.html',
   styleUrl: './pay-order.component.scss'
 })
