7c78c999f929d257e6d02e539ae46942224c55ef
Leticia Orive <leticia.orive@accenture.com>
2025-01-16 09:53:36 +0100

    crearse una cuenta de stripe

--- 07-burguer-queen-app/src/environments/environment.ts
+++ 07-burguer-queen-app/src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
-    urlServer: 'http://localhost:3000/api'
+    urlServer: 'http://localhost:3000/api',
+
+    stripe: {
+        publishKey: 'pk_test_51QhoZBKv05gOvxzWrQTB7Ar4uxWOJuyfbcAs6DDmXxT3VLLf1Dp0TQgVkzYiwo9IIrl2MTGb9xlr98ZzXyTacjME00OdNAG5WO',
+        secretKey: 'sk_test_51QhoZBKv05gOvxzW0egXp27hZckzNVpsCWigrkj6DuyOtGER1uO4JYXeSCUFHvS32iMkvq5Uy0c4b19R9KaAc7vG00g8iM80ut',
+        customer_id: 'cus_Rb0oMOO58zzIOt'
+
+    }
 };
