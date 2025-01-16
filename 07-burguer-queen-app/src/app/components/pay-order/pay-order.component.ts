3a69d816ec688b21b4f225c1e5acfb1b728586d5
Leticia Orive <leticia.orive@accenture.com>
2025-01-16 16:27:40 +0100

    cambiar de idioma nuestro formulario stripe dinamico

--- 07-burguer-queen-app/src/app/components/pay-order/pay-order.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/pay-order.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject, ViewChild } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { OrderStatusComponent } from './components/order-status/order-status.component';
 import { injectStripe, StripeElementsDirective, StripePaymentElementComponent } from 'ngx-stripe';
 import { environment } from '../../../environments/environment.development';
 import {
-  StripeElementsOptions, 
-  StripePaymentElementOptions
+  StripeElementsOptions,
+  StripePaymentElementOptions,
+  StripeElementLocale
 } from '@stripe/stripe-js';
 import { StripeService } from '../../services/stripe.service';
 import { ICreatePaymentIntent } from '../../models/create-payment-intent.model';
@@ -17,13 +18,12 @@ import { StepperSelectionEvent } from '@angular/cdk/stepper';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { IOrder } from '../../models/order.model';
 import { AuthService } from '../../services/auth.service';
-import { first, switchMap } from 'rxjs';
+import { first, Subject, switchMap, takeUntil } from 'rxjs';
 import { IAuthCredentials } from '../../models/auth-credentials';
 import { OrdersService } from '../../services/orders.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogTicketComponent } from '../dialogs/dialog-ticket/dialog-ticket.component';
-
 
 @Component({
   selector: 'app-pay-order',
@@ -45,8 +45,13 @@ import { DialogTicketComponent } from '../dialogs/dialog-ticket/dialog-ticket.co
 })
 export class PayOrderComponent {
 
+  // Obtengo el template del formulario de pago (ngx-stripe-payment)
   @ViewChild(StripePaymentElementComponent)
   paymentElement!: StripePaymentElementComponent
+
+  // Obtengo el template del formulario (ngx-stripe-elements)
+  @ViewChild(StripeElementsDirective)
+  formElement!: StripeElementsDirective
 
   private stripeService = inject(StripeService);
   private userOrderService = inject(UserOrderService);
@@ -55,11 +60,13 @@ export class PayOrderComponent {
   private dialog = inject(MatDialog);
   private authService = inject(AuthService);
   private orderService = inject(OrdersService);
-  private router = inject(Router);
+  private router = inject(Router)
 
   public stripe = injectStripe(environment.stripe.publishKey);
+
+  // Opciones formulario
   public elementsOptions: StripeElementsOptions = {
-    locale: 'en',
+    locale: (this.translateService.currentLang || this.translateService.defaultLang) as StripeElementLocale,
     appearance: {
       theme: 'flat'
     }
@@ -73,25 +80,53 @@ export class PayOrderComponent {
     }
   };
 
+  // Total de la orden
   public totalOrderSignal = this.userOrderService.totalOrderSignal;
+  // Ultimo total, usado para saber si tenemos que pedir un nuevo paymentIntent
   private lastTotal: number = 0;
+  // Subject para desuscribirse
+  private unsubscribe$ = new Subject<void>();
 
-  createPaymentIntent(event: StepperSelectionEvent){
+  ngOnInit(){
 
-    if(event.selectedIndex == 1 && (!this.elementsOptions.clientSecret || this.lastTotal != this.totalOrderSignal())){
+    // Controlamos el cambio de idioma para el formulario
+    this.translateService.onLangChange.asObservable().pipe(takeUntil(this.unsubscribe$)).subscribe({
+      next: (langEvent: LangChangeEvent) => {
+        this.elementsOptions.locale = langEvent.lang as StripeElementLocale;
+        if(this.formElement){
+          // actualizo el formulario
+          this.formElement.update(this.elementsOptions);
+        }
+      }
+    })
+
+  }
+
+  /**
+   * Creo el paymentIntent para generar el clientSecret
+   * @param event 
+   */
+  createPaymentIntent(event: StepperSelectionEvent) {
+
+    // Si estoy en el step de pago y si no hay clientSecret o el total ha cambiado, entro
+    if (event.selectedIndex == 1 && (!this.elementsOptions.clientSecret || this.lastTotal != this.totalOrderSignal())) {
 
       this.lastTotal = this.totalOrderSignal();
+      // Necesario para stripe
       const amount = this.totalOrderSignal() * 100;
 
+      // Preparo el paymentIntent
       const paymentIntent: ICreatePaymentIntent = {
         secretKey: environment.stripe.secretKey,
         amount: +amount.toFixed(0),
         currency: 'EUR',
         customer_id: environment.stripe.customer_id
       }
-  
+
+      // Creo el paymentIntent
       this.stripeService.createPaymentSheet(paymentIntent).subscribe({
-        next: (paymentIntent: IPayment) =>{
+        next: (paymentIntent: IPayment) => {
+          // Obtengo el clientSecret
           this.elementsOptions.clientSecret = paymentIntent.paymentIntentClientSecret;
         }
       })
@@ -99,74 +134,91 @@ export class PayOrderComponent {
 
   }
 
-  payOrder(){
-    
+  /**
+   * Confirmo el pago y crea la orden
+   */
+  payOrder() {
+
+    // Confirmo el pago
     this.stripe
-    .confirmPayment({
-      elements: this.paymentElement.elements,
-      redirect: 'if_required'
-    })
-    .subscribe(result => {
-      if (result.error) {
-        this.snackBar.open(
-          this.translateService.instant('label.payment.error'),
-          this.translateService.instant('label.error'),
-          {
-            horizontalPosition: 'center',
-            verticalPosition: 'bottom',
-            duration: 5000
-          }
-        )
-      } else {
-        if (result.paymentIntent.status === 'succeeded') {
+      .confirmPayment({
+        elements: this.paymentElement.elements,
+        redirect: 'if_required'
+      })
+      .subscribe(result => {
+        if (result.error) {
           this.snackBar.open(
-            this.translateService.instant('label.payment.ok'),
-            this.translateService.instant('label.ok'),
+            this.translateService.instant('label.payment.error'),
+            this.translateService.instant('label.error'),
             {
               horizontalPosition: 'center',
               verticalPosition: 'bottom',
               duration: 5000
             }
           )
-          this.createOrder();
+        } else {
+          // si todo ha ido bien, mostramos un mensaje y creamos la orden
+          if (result.paymentIntent.status === 'succeeded') {
+            this.snackBar.open(
+              this.translateService.instant('label.payment.ok'),
+              this.translateService.instant('label.ok'),
+              {
+                horizontalPosition: 'center',
+                verticalPosition: 'bottom',
+                duration: 5000
+              }
+            )
+            this.createOrder();
+          }
         }
-      }
-    });
+      });
 
   }
 
-  createOrder(){
+  /**
+   * Crea la orden en nuestro backend
+   */
+  createOrder() {
+
+    // Datos de entrada
     const order: IOrder = {
-      address: 'Calle Falsa 123',
-      user:{
-        //El email tiene que ser ese, a no ser que os creeis otro en el backend
-        email: 'test@gmail.com',
+      address: 'Calle falsa 123',
+      user: {
+        email: 'test@gmail.com', // IMPORTANTE que se llame igual
         password: '123456'
       },
-      products: this.userOrderService.productsSignals(),
+      products: this.userOrderService.productsSignals()
     }
 
-   this.authService.login(order.user).pipe(
-      switchMap((data: IAuthCredentials) => this.orderService.createOrder(order, data.accessToken))
+    // Necesario para poder insertar ordenes
+    this.authService.login(order.user).pipe(
+      switchMap((data: IAuthCredentials) => 
+        // Creo la orden
+        this.orderService.createOrder(order, data.accessToken))
     ).subscribe({
       next: (order: IOrder) => {
         console.log(order);
+        // reseteo la orden
         this.userOrderService.resetOrder();
+        // Abro el dialogo para mostrar el ticket
+        // Cuando se cierra volvemos a las categorias
         this.dialog.open(DialogTicketComponent, {
           data: {
             ticket: order.ticket
           }
         }).afterClosed().pipe(first()).subscribe({
           next: () => {
-            this.router.navigateByUrl('/categories');
+            this.router.navigateByUrl('/categories')
           }
         })
-        
-
       }
     })
-    
 
+  }
+
+  ngOnDestroy(){
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 
