b8fc3580527369bbd2c25551a3989c3c62ad8a4e
Leticia Orive <leticia.orive@accenture.com>
2025-01-16 08:19:38 +0100

    mostrar dialogo extra

---
+++ 07-burguer-queen-app/src/app/components/dialogs/dialog-extras/dialog-extras.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-extras',
+  imports: [],
+  templateUrl: './dialog-extras.component.html',
+  styleUrl: './dialog-extras.component.scss'
+})
+export class DialogExtrasComponent {
+
+}
--- 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/components/order-status/order-status.component.ts
@@ -7,6 +7,9 @@ import { MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { IProduct } from '../../../../models/product.model';
 import { Router } from '@angular/router';
+import { IQuantityProduct } from '../../../../models/quantity-product.model';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogExtrasComponent } from '../../../dialogs/dialog-extras/dialog-extras.component';
 
 @Component({
   selector: 'app-order-status',
@@ -34,12 +37,15 @@ export class OrderStatusComponent {
   private UserOrderService= inject(UserOrderService) ;
   //injecto el router para poder navegar a la pagina de categorias
   private router = inject(Router);
+  private dialog = inject(MatDialog);
+
 
   public productsSignal = this.UserOrderService.productsSignals;
   public totalOrderSignal = this.UserOrderService.totalProductsSignals;
   public displayedColumns: string[] = ['name', 'price', 'quantity', 'total'];
 
-  oneLessProduct(product: IProduct) {
+  oneLessProduct(event: MouseEvent, product: IProduct) {
+    event.stopPropagation();
     this.UserOrderService.oneLessProduct(product);
 
     //por si el producto se queda a cero y desaparece que nos derive a la pagina de categorias
@@ -47,8 +53,22 @@ export class OrderStatusComponent {
       this.router.navigateByUrl('/categories')
     }
   }
-  oneMoreProduct(product: IProduct) {
+  oneMoreProduct(event: MouseEvent, product: IProduct) {
+    event.stopPropagation();
     this.UserOrderService.oneMoreProduct(product);
+  }
+  showExtras(row: IQuantityProduct){
+    console.log(row);
+    if(row.product.extras){
+
+      const extraBlocks = row.product.extras.flatMap((extra) => extra.blocks);
+
+      this.dialog.open(DialogExtrasComponent, {
+        data: {
+          extraBlocks
+        }
+      });
+    }
   }
 
 }
