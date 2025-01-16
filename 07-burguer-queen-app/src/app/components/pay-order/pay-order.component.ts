cfae4bbcdb9cc8db2bd1cbb3de6475ff57626190
Leticia Orive <leticia.orive@accenture.com>
2025-01-16 10:24:21 +0100

    crear nuestro formulario stripe

--- 07-burguer-queen-app/src/app/components/pay-order/pay-order.component.ts
+++ 07-burguer-queen-app/src/app/components/pay-order/pay-order.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-
-import {MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious} from '@angular/material/stepper';
-
+import { MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper';
 import { TranslateModule } from '@ngx-translate/core';
-import { OrderStatusComponent } from "./components/order-status/order-status.component";
+import { OrderStatusComponent } from './components/order-status/order-status.component';
+import { injectStripe, StripeElementsDirective, StripePaymentElementComponent } from 'ngx-stripe';
+import { environment } from '../../../environments/environment.development';
+import {
+  StripeElementsOptions, 
+  StripePaymentElementOptions
+} from '@stripe/stripe-js';
+
 @Component({
   selector: 'app-pay-order',
+  standalone: true,
   imports: [
     TranslateModule,
     MatStepper,
@@ -15,11 +21,35 @@ import { OrderStatusComponent } from "./components/order-status/order-status.com
     MatStepperNext,
     MatStepperPrevious,
     MatButton,
-    OrderStatusComponent
-],
+    OrderStatusComponent,
+    StripeElementsDirective,
+    StripePaymentElementComponent
+  ],
   templateUrl: './pay-order.component.html',
   styleUrl: './pay-order.component.scss'
 })
 export class PayOrderComponent {
 
+  public stripe = injectStripe(environment.stripe.publishKey);
+  public elementsOptions: StripeElementsOptions = {
+    locale: 'en',
+    appearance: {
+      theme: 'flat'
+    }
+  };
+  public paymentElementOptions: StripePaymentElementOptions = {
+    layout: {
+      type: 'tabs',
+      defaultCollapsed: false,
+      radios: false,
+      spacedAccordionItems: false
+    }
+  };
+
+  payOrder(){
+    
+  }
+
+
 }
+
