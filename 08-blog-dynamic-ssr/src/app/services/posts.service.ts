726058e592d7ff4216ab43e66d552e83f606d4e3
Leticia Orive <leticia.orive@accenture.com>
2025-01-20 15:17:07 +0100

    obtener entrada

--- 08-blog-dynamic-ssr/src/app/services/posts.service.ts
+++ 08-blog-dynamic-ssr/src/app/services/posts.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { IPage } from '../models/page.model';
+import { IPost } from '../models/post.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  private URL_BASE = `${environment.urlServer}/v1/posts`;
+
+  getPosts(page: number = 1, q?: string, category?: string): Observable<IPage<IPost>>{
+
+    let url = `${this.URL_BASE}/published?page=${page}&size=4&sortby=publishedDate&sort=DESC`
+
+    if(q){
+      url += `q=${q}`;
+    }
+
+    if(category){
+      url += `category=${category}`;
+    }
+
+    return this.http.get<IPage<IPost>>(url);
+
+  }
+
 }
