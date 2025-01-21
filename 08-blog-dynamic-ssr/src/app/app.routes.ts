a8894d8f6e1e88e51e04b10fba6c77f98732d5f0
Leticia Orive <leticia.orive@accenture.com>
2025-01-21 09:40:51 +0100

    rutas lazy loading

--- 08-blog-dynamic-ssr/src/app/app.routes.ts
+++ 08-blog-dynamic-ssr/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/posts/posts.component').then(c => c.PostsComponent),
+        
+    },
+    {
+        path: 'category/:id/:name',
+        loadComponent: () => import('./components/posts/posts.component').then(c => c.PostsComponent),
+        
+    },
+    {
+        path: 'post/:id/:name',
+        loadComponent: () => import('./components/post/post.component').then(c => c.PostComponent),
+       
+    },
+    {
+        path: 'search',
+        loadComponent: () => import('./components/posts/posts.component').then(c => c.PostsComponent),
+        
+    }
+];
+
