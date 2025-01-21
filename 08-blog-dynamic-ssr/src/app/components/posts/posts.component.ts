97ad6442c1fd5f22c35433b74f848c86196e3cda
Leticia Orive <leticia.orive@accenture.com>
2025-01-21 13:11:59 +0100

    mostrar ultimos posts

--- 08-blog-dynamic-ssr/src/app/components/posts/posts.component.ts
+++ 08-blog-dynamic-ssr/src/app/components/posts/posts.component.ts
@@ -1,16 +1,22 @@
+import { DatePipe, SlicePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Data, ParamMap } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
+import { IPage } from '../../models/page.model';
+import { IPost } from '../../models/post.model';
 
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [DatePipe, SlicePipe],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
 export class PostsComponent {
   private route = inject(ActivatedRoute);
+
+  public pagination!: IPage<IPost>;
+  public posts: IPost[] = [];
 
   
   ngOnInit(){
@@ -19,6 +25,8 @@ export class PostsComponent {
     this.route.data.subscribe({
       next: (data: Data) => {
         console.log(data['posts']);
+        this.pagination = data['posts'];
+        this.posts = this.pagination.content;
         
       }
     });
