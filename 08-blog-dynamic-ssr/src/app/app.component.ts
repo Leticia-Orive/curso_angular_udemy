a1a51fe91984f49f13cb11df760a79affe794cab
Leticia Orive <leticia.orive@accenture.com>
2025-01-21 08:47:14 +0100

    creacion y borrar codigo de proyecto

--- 08-blog-dynamic-ssr/src/app/app.component.ts
+++ 08-blog-dynamic-ssr/src/app/app.component.ts
@@ -1,35 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
-  private meta = inject(Meta)
-
-  ngOnInit(){
-
-    this.meta.addTags([
-      {
-        name: 'description',
-        content: 'Disco duro de roer es una web donde encontrarás tutoriales y ejercicios sobre informática, centrándose sobre todo en programación, para aprender más.'
-      },
-      {
-        name: 'keywords',
-        content: 'disco duro de roer,programacion,tutoriales,aprender,programar,aprender a programar,informatica'
-      }
-    ])
-
-  }
-
-
-
-
+  title = '08-blog-dynamic-ssr';
 }
