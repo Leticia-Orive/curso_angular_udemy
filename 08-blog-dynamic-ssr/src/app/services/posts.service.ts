3a74c4eb424cec45bbfc31b697f0620bf1b2f8d5
Leticia Orive <leticia.orive@accenture.com>
2025-01-21 12:30:14 +0100

    constantes

--- 08-blog-dynamic-ssr/src/app/services/posts.service.ts
+++ 08-blog-dynamic-ssr/src/app/services/posts.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 import { IPage } from '../models/page.model';
 import { IPost } from '../models/post.model';
+import { SIZE_PAGINATION, SORT_BY_PAGINATION, SORT_PAGINATION } from '../constants';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,7 @@ export class PostsService {
   getPosts(page: number = 1, q?: string, category?: string): Observable<IPage<IPost>>{
 
     // url base
-    let url = `${this.URL_BASE}/published?page=${page}&size=4&sortby=publishedDate&sort=DESC`
+    let url = `${this.URL_BASE}/published?page=${page}&size=${SIZE_PAGINATION}&sortby=${SORT_BY_PAGINATION}&sort=${SORT_PAGINATION}`
 
     
     // Busqueda de texto
