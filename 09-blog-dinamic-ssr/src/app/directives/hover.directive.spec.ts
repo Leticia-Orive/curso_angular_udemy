e66501e26a63021c1a8fc045c30c47858b47ac57
Leticia Orive <leticia.orive@accenture.com>
2025-01-22 11:49:18 +0100

    congigurar nuevo proyecto y borrar una prueba

--- 09-blog-dinamic-ssr/src/app/directives/hover.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HoverDirective } from "./hover.directive";
-
-
-describe('HoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
