0f1a04c0e8f78f5284facef3ef26ad5fc9ee29be
Leticia Orive <leticia.orive@accenture.com>
2025-01-22 10:57:39 +0100

    buscador de entradas

--- 09-blog-dinamic-ssr/src/app/components/search/search.component.ts
+++ 09-blog-dinamic-ssr/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-search',
@@ -8,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  private router = inject(Router)
+
+  public showSearch: boolean = false;
+
+  /**
+   * Muestra el buscador
+   */
+  openInputSearch(){
+    this.showSearch = true;
+  }
+
+  /**
+   * Oculta el buscador
+   */
+  closeInputSearch(){
+    this.showSearch = false;
+  }
+
+  /**
+   * Buscamos las entradas
+   * @param value 
+   */
+  searchPosts(value: string){
+    this.router.navigate(['search'], {
+      queryParams: { q: value }
+    })
+    this.closeInputSearch();
+  }
+
 }
+
