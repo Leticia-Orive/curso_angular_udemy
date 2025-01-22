c0d5695ef3d24ed07b3e470c5f8c1407ae05b29d
Leticia Orive <leticia.orive@accenture.com>
2025-01-22 09:43:02 +0100

    mostrar nuestra entrada

--- 09-blog-dinamic-ssr/src/app/components/post/post.component.ts
+++ 09-blog-dinamic-ssr/src/app/components/post/post.component.ts
@@ -2,10 +2,13 @@ import { Component, inject } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 import { IPost } from '../../models/post.model';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { SanitizeHtmlPipe } from '../../pipes/sanitize-html.pipe';
+import { EnvironmentPipe } from '../../pipes/environment.pipe';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [DatePipe, SanitizeHtmlPipe, NgOptimizedImage, EnvironmentPipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
