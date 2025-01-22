fc24b0bf1a87a131b52a1aa86bea120d33a2e72a
Leticia Orive <leticia.orive@accenture.com>
2025-01-22 16:42:41 +0100

    IAuth y IAuthToken

---
+++ 09-dashboard-app/src/app/models/auth.model.ts
@@ -0,0 +1,11 @@
+export interface IAuth{
+    email: string;
+    password: string;
+}
+export interface IAuthToken{
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpires: number;
+    refreshTokenExpires: number;
+
+}
