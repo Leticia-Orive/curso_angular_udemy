3c4cd493029eb33a1b9b5143c35545d02010cc4d
Leticia Orive <leticia.orive@accenture.com>
2025-01-22 16:17:42 +0100

    Instalar @ngxs/store y @ngxs/cli

--- 09-dashboard-app/src/app/app.config.ts
+++ 09-dashboard-app/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {provideToastr} from 'ngx-toastr';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimations(),//requiered for animations
-    provideToastr(), //Toastr providers
+    provideToastr(),//Toastr providers
+     provideStore([],), 
   ]
 };
