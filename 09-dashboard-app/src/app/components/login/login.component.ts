cd059b26744ffb8137bbd96db99642a1e618a9c7
Leticia Orive <leticia.orive@accenture.com>
2025-01-23 09:34:20 +0100

    login

--- 09-dashboard-app/src/app/components/login/login.component.ts
+++ 09-dashboard-app/src/app/components/login/login.component.ts
@@ -1,23 +1,51 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IAuth } from '../../models/auth.model';
+import { Store } from '@ngxs/store';
+import { LoginAction } from '../../state/auth/auth.actions';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
   private formBuilder = inject(FormBuilder);
+  private store = inject(Store);
+  private toastrService = inject(ToastrService)
 
   public formLogin: FormGroup = new FormGroup({});
 
   ngOnInit(){
     this.formLogin = this.formBuilder.group({
       email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-    });
+      password: new FormControl('', Validators.required)
+    })
   }
-  checkLogin(){} 
+
+  checkLogin(){
+
+    const authCredentials: IAuth = this.formLogin.value;
+
+    this.store.dispatch(new LoginAction({ authCredentials })).subscribe({
+      next: () => {
+        this.toastrService.success(
+          'Logueado con éxito',
+          'Éxito'
+        );
+      }, error: () => {
+        this.toastrService.error(
+          'Error al loguearse',
+          'Error'
+        );
+      }
+    })
+
+  }
+
 
 }
--- 09-dashboard-app/src/app/services/auth.service.ts
+++ 09-dashboard-app/src/app/services/auth.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { IAuth, IAuthToken } from '../models/auth.model';
 import { first } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-private http= inject(HttpClient);
-private URL_BASE = '${environment.urlServer}/v1/auth';
+  private http = inject(HttpClient);
+  private URL_BASE = `${environment.urlServer}/v1/auth`
 
-login(authCredentials: IAuth){
-  return this.http.post<IAuthToken>('${this.URL_BASE}/login', authCredentials).pipe(first());
-}
+  login(authCredentials: IAuth){
+    return this.http.post<IAuthToken>(`${this.URL_BASE}/login`, authCredentials).pipe(first())
+  }
 }
