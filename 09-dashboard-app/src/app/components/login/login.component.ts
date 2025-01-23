95597edc194c807ea49dcd2302f7f0446c72bd5c
Leticia Orive <leticia.orive@accenture.com>
2025-01-23 08:57:45 +0100

    formulario login prepararlo

--- 09-dashboard-app/src/app/components/login/login.component.ts
+++ 09-dashboard-app/src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
+
+  public formLogin: FormGroup = new FormGroup({});
+
+  ngOnInit(){
+    this.formLogin = this.formBuilder.group({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+  checkLogin(){} 
 
 }
