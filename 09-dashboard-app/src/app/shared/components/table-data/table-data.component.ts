e0813e1277fff881fa24439c76f89395579da39a
Leticia Orive <leticia.orive@accenture.com>
2025-01-24 13:22:53 +0100

    filas selecionables

--- 09-dashboard-app/src/app/shared/components/table-data/table-data.component.ts
+++ 09-dashboard-app/src/app/shared/components/table-data/table-data.component.ts
@@ -1,18 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { booleanAttribute, Component, EventEmitter, Input, Output } from '@angular/core';
 import { IColumn } from './models/column.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table-data',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './table-data.component.html',
   styleUrl: './table-data.component.scss'
 })
 export class TableDataComponent<T extends { [key: string]: any}> {
   @Input() columns: IColumn[] = [];
   @Input() rows: T[] = [];
+  @Input ({transform: booleanAttribute}) selectable = false; // Muestra los checkboxes para seleccionar filas
 
   @Output() sortData = new EventEmitter<IColumn>();
 
+  public selectedRows: boolean[] = [];
+  public allChecked = false;
+
+  ngOnInit(){
+    this.selectedRows = [...Array(this.rows.length)].map(value => false);
+  }
 
   /**
    * Enviamos la columna actualizada
@@ -27,6 +35,9 @@ export class TableDataComponent<T extends { [key: string]: any}> {
     }))
     this.sortData.emit(column);
     
+  }
+  toggleSelectAll(){
+    this.selectedRows = this.selectedRows.map(() => this.allChecked);
   }
 
 
