c1f70d39af10c86bbaa8533ab11f4bb79ac6bffa
Leticia Orive <leticia.orive@accenture.com>
2025-01-28 09:40:41 +0100

    Forrmulario por categorias

---
+++ 09-dashboard-app/src/app/shared/components/category-form/category-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { ICategory } from '../../../models/category.model';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-category-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './category-form.component.html',
+  styleUrl: './category-form.component.scss'
+})
+export class CategoryFormComponent {
+
+  @Input() category?: ICategory;
+
+  private formBuilder = inject(FormBuilder);
+  public formCategory: FormGroup = new FormGroup({});
+
+  @Output() submitForm = new EventEmitter<ICategory>();
+
+  ngOnInit(){
+    this.formCategory = this.formBuilder.group({
+      _id: new FormControl(this.category?._id),
+
+      name: new FormControl(this.category ? this.category.name : '', Validators.required),
+
+      order: new FormControl(this.category ? this.category.order : 0),
+
+      parent: new FormControl(this.category && this.category.parent ? this.category.parent as ICategory : null)
+
+    })
+  }
+
+  submit(){
+    const category = this.formCategory.value as ICategory;
+    this.submitForm.emit(category);
+  }
+
+}
