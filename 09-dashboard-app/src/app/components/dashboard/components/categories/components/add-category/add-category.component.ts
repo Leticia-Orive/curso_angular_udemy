aa9e7eca698fa3a52a5ca88b06aa67266d8ed071
Leticia Orive <leticia.orive@accenture.com>
2025-01-28 13:15:06 +0100

    añadir categorias

--- 09-dashboard-app/src/app/components/dashboard/components/categories/components/add-category/add-category.component.ts
+++ 09-dashboard-app/src/app/components/dashboard/components/categories/components/add-category/add-category.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CategoryFormComponent } from '../../../../../../shared/components/category-form/category-form.component';
 import { ICategory } from '../../../../../../models/category.model';
+import { Store } from '@ngxs/store';
+import { CreateCategoryAction } from '../../../../../../state/categories/categories.actions';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,12 +14,33 @@ import { ICategory } from '../../../../../../models/category.model';
   styleUrl: './add-category.component.scss'
 })
 export class AddCategoryComponent {
+
+  private store = inject(Store);
+  private toastrService = inject(ToastrService);
+  private router = inject(Router);
   /**
    * Creamos una categoria
    * @param category 
    */
   createCategory(category: ICategory){
     console.log(category);
+    this.store.dispatch(new CreateCategoryAction({category})).subscribe({
+      next: () => {
+        this.toastrService.success(
+          'Categoria añadida correctamente',
+          'Exito'
+        );
+        // Volvemos a las categorias
+        this.router.navigate(['/dashboard', 'categories']);
+      }, error: (error) => {
+        console.log(error);
+        this.toastrService.error(
+          'Ha ocurrido un error al añadir la categoria',
+          'Error'
+        );
+
+      }
+    });
   }
 
 }
