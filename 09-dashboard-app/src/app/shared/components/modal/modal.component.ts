674c028f6a3bcd0763354a1c07581c1448188656
Leticia Orive <leticia.orive@accenture.com>
2025-01-29 13:21:52 +0100

    personalizar nuestro modal

--- 09-dashboard-app/src/app/shared/components/modal/modal.component.ts
+++ 09-dashboard-app/src/app/shared/components/modal/modal.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
 import { Modal} from 'bootstrap'
+import { IModal } from './models/modal.model';
 
 @Component({
-  selector: 'app-modal',
+  standalone: true,
   imports: [],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
@@ -11,10 +12,34 @@ export class ModalComponent {
 
   @ViewChild("modal", { static: true }) modal!: ElementRef;
 
+  public modalData?: IModal;
+  public close: EventEmitter<void> = new EventEmitter<void>();
+  public accept: EventEmitter<void> = new EventEmitter<void>();
+
   private modalInstance!: Modal;
 
   ngAfterViewInit() {
     this.modalInstance = new Modal(this.modal.nativeElement);
+  }
+
+  open(){
+    if(this.modalInstance){
+      this.modalInstance.show();
+    }
+  }
+
+  hide(){
+    if(this.modalInstance){
+      this.modalInstance.hide();
+      this.close.emit();
+    }
+  }
+
+  confirm(){
+    if(this.modalInstance){
+      this.modalInstance.hide();
+      this.accept.emit();
+    }
   }
 
 }
