78429ecc0b7585eab7b1208e6e036b560a89dfa7
Leticia Orive <leticia.orive@accenture.com>
2025-01-30 09:53:48 +0100

    Type TSort

--- 09-dashboard-app/src/app/state/posts/posts.actions.ts
+++ 09-dashboard-app/src/app/state/posts/posts.actions.ts
@@ -1,4 +1,6 @@
+import { TSort } from "../../shared/components/table-data/types/sort.type";
+
 export class GetPostsAction {
   static readonly type = '[Posts] Get Posts paginated';
-  constructor(public payload: { page: number, q?: string, sortBy?: string, sort?: string }) { }
+  constructor(public payload: { page: number, q?: string, sortBy?: string, sort?: TSort }) { }
 }
