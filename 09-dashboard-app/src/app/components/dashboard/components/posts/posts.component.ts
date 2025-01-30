ff535cc61a5586193ff22d0852c777741e55f89a
Leticia Orive <leticia.orive@accenture.com>
2025-01-30 09:03:34 +0100

    mostrar nuestro posts

--- 09-dashboard-app/src/app/components/dashboard/components/posts/posts.component.ts
+++ 09-dashboard-app/src/app/components/dashboard/components/posts/posts.component.ts
@@ -4,10 +4,17 @@ import { Observable } from 'rxjs';
 import { IPage } from '../../../../models/page.model';
 import { IPost } from '../../../../models/post.model';
 import { PostsState } from '../../../../state/posts/posts.state';
+import { TableDataComponent } from '../../../../shared/components/table-data/table-data.component';
+import { AsyncPipe } from '@angular/common';
+import { GetPostsAction } from '../../../../state/posts/posts.actions';
+import { RouterLink } from '@angular/router';
+import { IColumn } from '../../../../shared/components/table-data/models/column.model';
+import { IAction } from '../../../../shared/components/table-data/models/action.model';
+import { TAction } from '../../../../types';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [TableDataComponent, AsyncPipe, RouterLink],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
@@ -17,4 +24,62 @@ export class PostsComponent {
 
   // Paginacion de posts
   public pagination$: Observable<IPage<IPost> | null> = this.store.select(PostsState.pagination)
+
+  // parametros de busquedas
+  private sortBy = 'publishedDate'
+  private sort = 'DESC'
+  private page = 1;
+  private searchText = '';
+
+   // Columnas
+   public columns: IColumn[] = [
+    {
+      display: 'Titulo',
+      property: 'title',
+      canSort: true
+    },
+    {
+      display: 'Fecha publicación',
+      property: 'publishedDate',
+      sort: 'DESC',
+      canSort: true
+    },
+    {
+      display: 'Categorías',
+      property: 'categories',
+      canSort: false
+    }
+  ]
+   // Acciones disponibles
+   public actionsAvailables: IAction<TAction>[] = [
+    {
+      name: 'publish',
+      display: 'Publicar'
+    },
+    {
+      name: 'unpublish',
+      display: 'Despublicar'
+    },
+    {
+      name: 'delete',
+      display: 'Borrar'
+    }
+  ]
+
+
+  ngOnInit(){
+    this.getPosts()
+  }
+  /**
+   * Obtenemos los posts paginados
+   */
+  getPosts(){
+    this.store.dispatch(new GetPostsAction({
+      page: this.page,
+      q: this.searchText,
+      sort: this.sort,
+      sortBy: this.sortBy
+    }))
+  }
+
 }
