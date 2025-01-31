a17fbee3fb292aa198f89ac06c3edfa9e4db139c
Leticia Orive <leticia.orive@accenture.com>
2025-01-31 10:05:33 +0100

    añadir entrada

--- 09-dashboard-app/src/app/components/dashboard/components/posts/components/add-post/add-post.component.ts
+++ 09-dashboard-app/src/app/components/dashboard/components/posts/components/add-post/add-post.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { PostFormComponent } from '../../../../../../shared/components/post-form/post-form.component';
 import { IPost } from '../../../../../../models/post.model';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { ToastrService } from 'ngx-toastr';
+import { CreatePostAction } from '../../../../../../state/posts/posts.actions';
 
 
 @Component({
@@ -10,10 +14,29 @@ import { IPost } from '../../../../../../models/post.model';
   styleUrl: './add-post.component.scss'
 })
 export class AddPostComponent {
+  private store = inject(Store)
+  private router = inject(Router)
+  private toatrService = inject(ToastrService)
   
 
   createPost(post: IPost){
     console.log(post);
+
+    this.store.dispatch(new CreatePostAction({ post })).subscribe({
+      next: () => {
+        this.toatrService.success(
+          'Post añadido',
+          'Éxito'
+        )
+        this.router.navigate(['/dashboard', 'posts']);
+      }, error: (error) => {
+        console.error(error);
+        this.toatrService.error(
+          'Error al añadir el post',
+          'Error'
+        )
+      }
+    })
     
     
 
