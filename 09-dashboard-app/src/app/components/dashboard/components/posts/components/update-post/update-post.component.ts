ddcd0e48e13d960e06c112114c34b7e6e24b967f
Leticia Orive <leticia.orive@accenture.com>
2025-01-31 12:31:17 +0100

    Visualizar entrada para actualizar

--- 09-dashboard-app/src/app/components/dashboard/components/posts/components/update-post/update-post.component.ts
+++ 09-dashboard-app/src/app/components/dashboard/components/posts/components/update-post/update-post.component.ts
@@ -1,16 +1,19 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { PostsState } from '../../../../../../state/posts/posts.state';
 import { IPost } from '../../../../../../models/post.model';
 import { Observable } from 'rxjs/internal/Observable';
-import { ClearPostSelectedAction, GetPostByIdAction } from '../../../../../../state/posts/posts.actions';
+import { ClearPostSelectedAction, GetPostByIdAction} from '../../../../../../state/posts/posts.actions';
 
-import { AsyncPipe, JsonPipe } from '@angular/common';
-
+import { AsyncPipe } from '@angular/common';
+import { PostFormComponent } from '../../../../../../shared/components/post-form/post-form.component';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-update-post',
-  imports: [AsyncPipe, JsonPipe],
+  standalone: true,
+  imports: [AsyncPipe, PostFormComponent],
   templateUrl: './update-post.component.html',
   styleUrl: './update-post.component.scss'
 })
@@ -20,7 +23,8 @@ export class UpdatePostComponent {
   @Input() id!: string;
 
   private store = inject(Store)
- 
+  private router = inject(Router)
+  private toastrService = inject(ToastrService)
 
   // Observable para obtener el post
   public post$: Observable<IPost | null> = this.store.select(PostsState.postSelected)
@@ -29,7 +33,7 @@ export class UpdatePostComponent {
     this.store.dispatch(new GetPostByIdAction({ id: this.id }))
   }
 
-  
+ updatePost(post: IPost){}
 
   ngOnDestroy(){
     // limpiamos el post seleccionado
