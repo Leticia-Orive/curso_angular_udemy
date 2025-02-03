d1318be298b46078660671b2634b6ff1e65f550e
Leticia Orive <leticia.orive@accenture.com>
2025-02-03 15:46:54 +0100

    Preparando el componente create account

--- 10-control-expenses/src/app/components/create-account/create-account.component.ts
+++ 10-control-expenses/src/app/components/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+
+  private formBuilder = inject(FormBuilder)
+
+  public formCreateAccount: FormGroup = new FormGroup({})
+
+  ngOnInit(){
+    this.formCreateAccount = this.formBuilder.group({
+      
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+    })
+  }
 
 }
