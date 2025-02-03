94232f397d45900d158df6412c77e99411cfaf15
Leticia Orive <leticia.orive@accenture.com>
2025-02-03 13:35:55 +0100

    Login usando AuthService

--- 10-control-expenses/src/app/services/auth.service.ts
+++ 10-control-expenses/src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/app';
+import { Auth, getAuth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { IAuthCredentials } from '../models/auth-credentials';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private afApp = inject(FirebaseApp)
+  private auth: Auth = getAuth(this.afApp)
 
-  constructor() { }
+  login(authCredentials: IAuthCredentials){
+    return signInWithEmailAndPassword(this.auth, authCredentials.email, authCredentials.password);
+  }
+
 }
