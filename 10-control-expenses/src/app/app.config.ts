7c2568b1268e0ae1f89e232daf4b5b764932f32d
Leticia Orive <leticia.orive@accenture.com>
2025-02-03 11:43:50 +0100

    Environments

--- 10-control-expenses/src/app/app.config.ts
+++ 10-control-expenses/src/app/app.config.ts
@@ -15,9 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideToastr(), 
     provideFirebaseApp(() => initializeApp
-    ({ projectId: "control-expenses-ddd75", appId: "1:163533585496:web:35238f455746938d21de2d", 
-      storageBucket: "control-expenses-ddd75.firebasestorage.app", apiKey: "AIzaSyCerwVtGhTEmWCVTRDn1mQVJCa-FiwTChc", 
-      authDomain: "control-expenses-ddd75.firebaseapp.com", messagingSenderId: "163533585496" })),
+    ()),
        provideAuth(() => getAuth()), 
        provideFirestore(() => getFirestore()),
     ]
---
+++ 10-control-expenses/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    firebase: {
+         projectId: "control-expenses-ddd75", 
+         appId: "1:163533585496:web:35238f455746938d21de2d", 
+        storageBucket: "control-expenses-ddd75.firebasestorage.app", 
+        apiKey: "AIzaSyCerwVtGhTEmWCVTRDn1mQVJCa-FiwTChc", 
+        authDomain: "control-expenses-ddd75.firebaseapp.com",
+        messagingSenderId: "163533585496"}
+};
