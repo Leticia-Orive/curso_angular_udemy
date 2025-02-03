9e2bd1ac494beb6a994d47e929e2fe3171ac93fd
Leticia Orive <leticia.orive@accenture.com>
2025-02-03 11:03:25 +0100

    rutas

--- 10-control-expenses/src/app/app.routes.ts
+++ 10-control-expenses/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { GraphicsComponent } from './components/graphics/graphics.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistriesComponent } from './components/registries/registries.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'registries',
+        component: RegistriesComponent,
+        
+    },
+    {
+        path: 'categories',
+        component: CategoriesComponent,
+        
+    },
+    {
+        path: 'graphics',
+        component: GraphicsComponent,
+       
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'create-account',
+        component: CreateAccountComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'registries'
+    }
+];
