a9ad8baf31dc7e40be1a509e81941f680433404f
Leticia Orive <leticia.orive@accenture.com>
2025-02-03 13:28:08 +0100

    preparar login

--- 10-control-expenses/src/app/components/login/login.component.ts
+++ 10-control-expenses/src/app/components/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder,  FormControl,  FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private formBuilder = inject(FormBuilder)
+
+  public formLogin: FormGroup = new FormGroup({});
+
+  ngOnInit(){
+    this.formLogin = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    })
+      
+  }
+
+  login(){}
 
 }
