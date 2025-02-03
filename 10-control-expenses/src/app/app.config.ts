7d0e391c3f13b931953f5282e7e3d7d522a2e5df
Leticia Orive <leticia.orive@accenture.com>
2025-02-03 11:33:57 +0100

    Instalar dependencias Firebase en Angular

--- 10-control-expenses/src/app/app.config.ts
+++ 10-control-expenses/src/app/app.config.ts
@@ -4,12 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimations(),
-    provideToastr(),
+    provideToastr(), 
+    provideFirebaseApp(() => initializeApp
+    ({ projectId: "control-expenses-ddd75", appId: "1:163533585496:web:35238f455746938d21de2d", 
+      storageBucket: "control-expenses-ddd75.firebasestorage.app", apiKey: "AIzaSyCerwVtGhTEmWCVTRDn1mQVJCa-FiwTChc", 
+      authDomain: "control-expenses-ddd75.firebaseapp.com", messagingSenderId: "163533585496" })),
+       provideAuth(() => getAuth()), 
+       provideFirestore(() => getFirestore()),
     ]
 };
