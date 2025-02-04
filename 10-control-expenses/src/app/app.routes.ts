8febc3b24e3f14d04d85b6ff95f95cfa4ea80587
Leticia Orive <leticia.orive@accenture.com>
2025-02-04 08:47:37 +0100

    AuthGuard de Firebase

--- 10-control-expenses/src/app/app.routes.ts
+++ 10-control-expenses/src/app/app.routes.ts
@@ -4,21 +4,36 @@ import { CreateAccountComponent } from './components/create-account/create-accou
 import { GraphicsComponent } from './components/graphics/graphics.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistriesComponent } from './components/registries/registries.component';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectToLogin = () => redirectUnauthorizedTo(['login'])
 
 export const routes: Routes = [
     {
         path: 'registries',
         component: RegistriesComponent,
+        canActivate: [AuthGuard],
+        data: {
+            authGuardPipe: redirectToLogin
+        }
         
     },
     {
         path: 'categories',
         component: CategoriesComponent,
+        canActivate: [AuthGuard],
+        data: {
+            authGuardPipe: redirectToLogin
+        }
         
     },
     {
         path: 'graphics',
         component: GraphicsComponent,
+        canActivate: [AuthGuard],
+        data: {
+            authGuardPipe: redirectToLogin
+        }
        
     },
     {
