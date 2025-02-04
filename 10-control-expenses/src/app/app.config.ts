78492a0df25b79afede3f4c1eb4d8411f2e72063
Leticia Orive <leticia.orive@accenture.com>
2025-02-04 14:08:53 +0100

    Animaciones en nuestro componente detail

--- 10-control-expenses/src/app/app.config.ts
+++ 10-control-expenses/src/app/app.config.ts
@@ -2,18 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { provideToastr } from 'ngx-toastr';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideAnimations(),
+    provideAnimationsAsync(),
     provideToastr(), 
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     
