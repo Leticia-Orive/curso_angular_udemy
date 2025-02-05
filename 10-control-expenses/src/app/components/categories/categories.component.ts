d29920aadebd1be933b48b4b79bd5a5123b6849d
Leticia Orive <leticia.orive@accenture.com>
2025-02-05 09:53:15 +0100

    Obtener categorias

--- 10-control-expenses/src/app/components/categories/categories.component.ts
+++ 10-control-expenses/src/app/components/categories/categories.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DetailComponent } from '../../shared/detail/detail.component';
 import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { CategoryService } from '../../services/category.service';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-categories',
-  imports: [DetailComponent, AddCategoryComponent],
+  imports: [DetailComponent, AddCategoryComponent, JsonPipe],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
 
+  private categoryService = inject(CategoryService)
+
   public showDetail: boolean = false;
+
+  // signals
+  public categoriesSignal = this.categoryService.categoriesSignal.asReadonly();
+
+  ngOnInit(){
+    this.categoryService.getCategories();
+  }
+  
 
 
 /**
--- 10-control-expenses/src/app/services/category.service.ts
+++ 10-control-expenses/src/app/services/category.service.ts
@@ -1,9 +1,10 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
-import { collection, doc, Firestore, getFirestore, setDoc } from '@angular/fire/firestore';
+import { collection, doc, DocumentData, Firestore, getDocs, getFirestore, query, QueryDocumentSnapshot, QuerySnapshot, setDoc } from '@angular/fire/firestore';
 import { ICategory } from '../models/category.model';
 import { AuthService } from './auth.service';
 import moment from 'moment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,37 @@ export class CategoryService {
   private afApp = inject(FirebaseApp)
   private database: Firestore = getFirestore(this.afApp)
   private authService = inject(AuthService);
+
+  // signals
+  public categoriesSignal: WritableSignal<ICategory[]> = signal<ICategory[]>([])
+ 
+
+getCategories() {
+  // Obtenemos la coleccion
+  const categoriesCollection = collection(this.database, 'categories');
+ 
+
+  // Creamos la query final
+  const queryCategories = query(
+    categoriesCollection,
+    
+  );
+
+  // Obtenemos las categorias
+
+  return getDocs(queryCategories).then((querySnapshot: QuerySnapshot<DocumentData, DocumentData>) => {
+    const categories: ICategory[] = [];
+    // recorremos las categorias
+    querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData, DocumentData>) => {
+      const data = doc.data() as ICategory;
+      categories.push(data);
+    })
+
+    this.categoriesSignal.set(categories);
+      
+  });
+
+}
 
 
   /**
