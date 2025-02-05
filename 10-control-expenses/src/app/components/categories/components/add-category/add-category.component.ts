b90312fca5679aae02c70a33ac1418837ea1915d
Leticia Orive <leticia.orive@accenture.com>
2025-02-05 09:22:44 +0100

    Añadir categoria

--- 10-control-expenses/src/app/components/categories/components/add-category/add-category.component.ts
+++ 10-control-expenses/src/app/components/categories/components/add-category/add-category.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormCategoryComponent } from '../../../../shared/form-category/form-category.component';
+import { ToastrService } from 'ngx-toastr';
+import { ICategory } from '../../../../models/category.model';
+import { CategoryService } from '../../../../services/category.service';
 
 @Component({
   selector: 'app-add-category',
@@ -8,5 +11,33 @@ import { FormCategoryComponent } from '../../../../shared/form-category/form-cat
   styleUrl: './add-category.component.scss'
 })
 export class AddCategoryComponent {
+
+  private categoryService = inject(CategoryService)
+  private toastrService = inject(ToastrService)
+
+  @Output() categorySaved: EventEmitter<void> = new EventEmitter<void>();
+
+   /**
+   * Creamos la categoria
+   * @param category 
+   */
+   createCategory(category: ICategory){
+
+    this.categoryService.createCategory(category).then( () => {
+      this.toastrService.success(
+        'Categoría creada',
+        'Éxito'
+      )
+      // Indicamos que hemos guardado la categoria
+      this.categorySaved.emit();
+    }, error => {
+      console.error(error);
+      this.toastrService.error(
+        'Error al crear la categoría',
+        'Error'
+      )
+    })
+
+  }
 
 }
