e86fd36e3ffa68fbfd5a23b5f608cb0c73a3b438
Leticia Orive <leticia.orive@accenture.com>
2025-02-07 13:36:33 +0100

    Mostrando nuestro formulario de registros

--- 10-control-expenses/src/app/components/registries/components/add-registry/add-registry.component.ts
+++ 10-control-expenses/src/app/components/registries/components/add-registry/add-registry.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormRegistryComponent } from '../../../../shared/form-registry/form-registry.component';
+import { TTypeRegistry } from '../../../../types';
+import { IRegistry } from '../../../../models/registry.model';
 
 @Component({
   selector: 'app-add-registry',
-  imports: [],
+  imports: [FormRegistryComponent],
   templateUrl: './add-registry.component.html',
   styleUrl: './add-registry.component.scss'
 })
 export class AddRegistryComponent {
+  @Input({ required: true }) typeRegistry!: TTypeRegistry;
+
+  createRegistry(registry: IRegistry){
+    
+  }
 
 }
--- 10-control-expenses/src/app/components/registries/registries.component.ts
+++ 10-control-expenses/src/app/components/registries/registries.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { DetailComponent } from '../../shared/detail/detail.component';
+import { AddRegistryComponent } from './components/add-registry/add-registry.component';
+import { TTypeRegistry } from '../../types';
+import { IRegistry } from '../../models/registry.model';
 
 @Component({
   selector: 'app-registries',
-  imports: [],
+  imports: [DetailComponent, AddRegistryComponent],
   templateUrl: './registries.component.html',
   styleUrl: './registries.component.scss'
 })
 export class RegistriesComponent {
+  public registrySelected!: IRegistry;
+  public showDetail = false;
+  public typeRegistry: TTypeRegistry = 'deposit';
+
+  /**
+   * Abrimos el detalle para crear el registro
+   * @param type 
+   */
+  openDetail(type: TTypeRegistry) {
+    this.typeRegistry = type;
+    this.showDetail = true;
+  }
+
+   /**
+   * Cerramos el detalle, si la acción es correcta, pedimos los datos de nuevo
+   * @param actionSuccess 
+   */
+   closeDetail(actionSuccess: boolean = false){
+    this.showDetail = false;
+    
+  }
 
 }
