7e016dc4c3dc028bb0f57919207a2e3bcda1d6ec
Leticia Orive <leticia.orive@accenture.com>
2025-02-11 09:07:56 +0100

    paginar registros

--- 10-control-expenses/src/app/services/registry.service.ts
+++ 10-control-expenses/src/app/services/registry.service.ts
@@ -1,9 +1,12 @@
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
-import {collection, doc, DocumentData, getDocs, getFirestore, orderBy, query, QueryConstraint, QuerySnapshot, setDoc, where } from '@angular/fire/firestore';
+import { collection, doc, DocumentData, endBefore, getDocs, getFirestore, limit, limitToLast, query, QueryConstraint, QuerySnapshot, setDoc, startAfter, where } from '@angular/fire/firestore';
 import { IRegistry } from '../models/registry.model';
 import { AuthService } from './auth.service';
 import moment from 'moment';
+import { orderBy } from '@firebase/firestore/lite';
+import { TDirection } from '../types';
+import { ITEMS_PAGINATION } from '../constants';
 
 @Injectable({
   providedIn: 'root'
@@ -14,77 +17,116 @@ export class RegistryService {
   private authService = inject(AuthService)
   private database = getFirestore(this.afApp)
 
-  // signals
   public registriesSignal: WritableSignal<IRegistry[]> = signal([]);
+  public nextRegistriesSignal: WritableSignal<boolean> = signal(false);
+  public previousRegistriesSignal: WritableSignal<boolean> = signal(false);
 
-  /**
-   * Obtiene los registros dado un filtro
-   * @param filter 
-   * @param direction 
-   * @returns 
-   */
-  getRegistries() {
+  private firstDocument?: DocumentData;
+  private lastDocument?: DocumentData;
 
-    // Obtiene la colección
+  getRegistries(direction: TDirection = null){
+
     const registryCollection = collection(this.database, 'registries');
 
-    // Query base
-    const queryConstraints = this.createQuery();
-    // Crear query
+    const queryConstraints = this.createQuery(direction);
+
     const queryRegistries = query(
       registryCollection,
       ...queryConstraints
     )
-    // Obtiene los registros 
+
     return getDocs(queryRegistries).then( (querrySnapshotRegistries: QuerySnapshot<DocumentData, DocumentData>) =>{
 
       const registries: IRegistry[] = [];
-      // recorremos los datos
+
       querrySnapshotRegistries.forEach( (doc) => {
         const data = doc.data() as IRegistry;
         registries.push(data);
       })
 
       this.registriesSignal.set(registries);
+
+      if(direction){
+
+        this.firstDocument = querrySnapshotRegistries.docs[0]
+        this.lastDocument = querrySnapshotRegistries.docs[querrySnapshotRegistries.docs.length - 1];
+
+        this.hasData('next');
+        this.hasData('previous');
+
+      }
       
       return registries;
 
     })
 
-    
 
   }
-  /**
-   * Crea una query base
-   * @param filter 
-   * @param direction 
-   * @returns 
-   */
-  private createQuery() {
 
-    // Obtengo el usuario actual
+  private createQuery(direction: TDirection = null){
+
     const user = this.authService.currentUser() as string;
 
-    // query base
     const queryConstraints: QueryConstraint[] = [
       orderBy('date', 'desc'),
       where("user", "==", user)
-    ];
+    ]
+
+    if(direction){
+      switch(direction){
+        case 'next':
+          queryConstraints.push(limit(ITEMS_PAGINATION))
+          if(this.lastDocument){
+            queryConstraints.push(startAfter(this.lastDocument))
+          }
+
+          break;
+        case 'previous':
+          queryConstraints.push(limitToLast(ITEMS_PAGINATION))
+          if(this.firstDocument){
+            queryConstraints.push(endBefore(this.firstDocument))
+          }
+          break;
+      }
+    }
+
     return queryConstraints;
   }
 
+  private async hasData(direction: TDirection){
 
-  async createRegistry(registry: IRegistry){
-     // Obtiene la colección
     const registryCollection = collection(this.database, 'registries');
-    const newRegistryRef = doc(registryCollection)
-// Actualizo la referencia del registro
-    registry.id = newRegistryRef.id
-    registry.user = this.authService.currentUser() as string
-    
-// Actualizo el registro
-    await setDoc(newRegistryRef, registry)
+    const queryHasDataContraints = this.createQuery(direction)
+
+    const queryHasData = query(
+      registryCollection,
+      ...queryHasDataContraints
+    )
+
+    const dataDocs = await getDocs(queryHasData);
+    const hasData = dataDocs.docs.length > 0;
+
+    switch(direction){
+      case 'next':
+        this.nextRegistriesSignal.set(hasData)
+        break;
+      case 'previous':
+        this.previousRegistriesSignal.set(hasData)
+        break;
+    }
+
   }
 
-  
+  async createRegistry(registry: IRegistry){
+
+    const registryCollection = collection(this.database, 'registries');
+    const newRegistryRef = doc(registryCollection)
+
+    registry.id = newRegistryRef.id
+    registry.user = this.authService.currentUser() as string
+
+    await setDoc(newRegistryRef, registry)
+
+  }
+
 }
