582aca983612c630d4dc66c9bde1bdc9c85a674b
Leticia Orive <leticia.orive@accenture.com>
2025-02-11 11:41:41 +0100

    Preparando nuestro Filter en el componente de registros

--- 10-control-expenses/src/app/components/registries/registries.component.ts
+++ 10-control-expenses/src/app/components/registries/registries.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { DetailComponent } from '../../shared/detail/detail.component';
 import { AddRegistryComponent } from './components/add-registry/add-registry.component';
 import { TTypeRegistry } from '../../types';
@@ -7,12 +7,15 @@ import { RegistryService } from '../../services/registry.service';
 import { AsyncPipe, DatePipe, NgClass } from '@angular/common';
 import { GetCategoryPipe } from '../../pipes/get-category.pipe';
 import { FilterComponent } from '../../shared/filter/filter.component';
+import { FormsModule } from '@angular/forms';
+import { IFilter } from '../../shared/filter/models/filter.model';
+import moment from 'moment';
 
 @Component({
   selector: 'app-registries',
   standalone: true,
   imports: [DetailComponent, AddRegistryComponent, DatePipe, NgClass, GetCategoryPipe,
-     AsyncPipe, FilterComponent],
+     AsyncPipe, FilterComponent, FormsModule],
   templateUrl: './registries.component.html',
   styleUrl: './registries.component.scss'
 })
@@ -23,6 +26,11 @@ export class RegistriesComponent {
   public registrySelected!: IRegistry;
   public showDetail = false;
   public typeRegistry: TTypeRegistry = 'deposit';
+  public filterSignal: WritableSignal<IFilter> = signal({
+    category: '',
+    dateStart: moment().startOf('month').format('YYYY-MM-DD'),
+    dateEnd: moment().endOf('month').format('YYYY-MM-DD')
+  })
 // signals
   public registriesSignal = this.registryService.registriesSignal.asReadonly();
   public nextRegistriesSignal = this.registryService.nextRegistriesSignal.asReadonly();
@@ -64,6 +72,11 @@ export class RegistriesComponent {
    */
   next(){
     this.registryService.getRegistries( 'next')
+  }
+  onFilter(filter: IFilter){
+    this.filterSignal.set(filter);
+    console.log(this.filterSignal());
+
   }
 
   ngOnDestroy(){
