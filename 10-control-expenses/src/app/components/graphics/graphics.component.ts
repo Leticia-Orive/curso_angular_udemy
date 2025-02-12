f8166b0c63368d2778d65ec67e28fbe9d434cdf4
Leticia Orive <leticia.orive@accenture.com>
2025-02-12 12:24:57 +0100

    crear nuestra grafica ejemplo

--- 10-control-expenses/src/app/components/graphics/graphics.component.ts
+++ 10-control-expenses/src/app/components/graphics/graphics.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Chart, ChartType } from 'chart.js/auto';
 @Component({
   selector: 'app-graphics',
   imports: [],
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './graphics.component.scss'
 })
 export class GraphicsComponent {
+  public chartBar!: Chart
+
+  ngOnInit(){
+    this.createChartBar();
+  }
+
+
+  createChartBar(){
+
+    this.chartBar = new Chart("chartBar", {
+      type: 'bar' as ChartType,
+      data: {
+        datasets: [{
+            type: 'bar',
+            label: 'Bar Dataset',
+            data: [10, 20, 30, 40]
+        }, {
+            type: 'line',
+            label: 'Line Dataset',
+            data: [15, 25, 15, 35],
+        }],
+        labels: ['January', 'February', 'March', 'April']
+      }
+    })
+
+
+  }
 
 }
