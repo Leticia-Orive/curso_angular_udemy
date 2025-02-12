208dbf7f45912f0c2c03de4f62515f04399b7938
Leticia Orive <leticia.orive@accenture.com>
2025-02-12 16:42:42 +0100

    Calcular la suma de gastos, ingresos y la diferencia

--- 10-control-expenses/src/app/components/graphics/graphics.component.ts
+++ 10-control-expenses/src/app/components/graphics/graphics.component.ts
@@ -1,8 +1,9 @@
 import { Component, effect, inject, Injector, signal, WritableSignal } from '@angular/core';
-import { Chart, ChartType, registry } from 'chart.js/auto';
+import { Chart, ChartType } from 'chart.js/auto';
 import { RegistryService } from '../../services/registry.service';
 import { IFilter } from '../../shared/filter/models/filter.model';
 import moment from 'moment';
+import 'moment/locale/es';
 import { IRegistry } from '../../models/registry.model';
 
 @Component({
@@ -44,20 +45,67 @@ export class GraphicsComponent {
   }
 
 
-  createChartBar(registries: IRegistry[]){  
+  createChartBar(registries: IRegistry[]){
 
     const registriesMonths = registries.reduce( (acc, registry) => {
       const month = moment(registry.date).format('MMMM').toLowerCase()
       if(!acc[month]){
-        acc[month].push(registry)
+        acc[month] = [];
       }
       acc[month].push(registry);
       return acc;
-
     }, {} as any)
 
     console.log(registriesMonths);
 
+    const dataDeposit: number[] = [];
+    const dataExpense: number[] = [];
+    const dataDifference: number[] = [];
+
+    const monthNames: string[] = [
+      "enero",
+      "febrero",
+      "marzo",
+      "abril",
+      "mayo",
+      "junio",
+      "julio",
+      "agosto",
+      "septiembre",
+      "octubre",
+      "noviembre",
+      "diciembre"
+    ]
+
+    for (const month of monthNames) {
+      if(registriesMonths[month]){
+        const deposit = registriesMonths[month].reduce( (acc: number, registry: IRegistry) => {
+          if(registry.type == 'deposit'){
+            return acc + registry.quantity
+          }
+          return acc;
+        }, 0);
+        const expense = registriesMonths[month].reduce( (acc: number, registry: IRegistry) => {
+          if(registry.type == 'expense'){
+            return acc + registry.quantity
+          }
+          return acc;
+        }, 0);
+        dataDeposit.push(deposit);
+        dataExpense.push(expense);
+        dataDifference.push(deposit - expense)
+      }else{
+        dataDeposit.push(0);
+        dataExpense.push(0);
+        dataDifference.push(0);
+      }
+    }
+    
+    console.log("Ingresos: ", dataDeposit);
+    console.log("Gastos: ", dataExpense);
+    console.log("Diferencia: ", dataDifference);
+    
+    
     this.chartBar = new Chart("chartBar", {
       type: 'bar' as ChartType,
       data: {
@@ -78,4 +126,3 @@ export class GraphicsComponent {
   }
 
 }
-
